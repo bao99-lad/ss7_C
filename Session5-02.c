b0154069d1d87505f6c9df5864a12728d9abc66b
bao99-lad <baotrantqb992006@gmail.com>
2024-12-18 11:51:28 +0700

    Add files via upload

---
+++ Session5-02.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int a = 1109, n;
+    while( n != a){
+        printf("Nhập vào một số: ");
+        scanf("%d", &n);
+    }
+    printf("Bạn đã nhập đúng số");
+}
